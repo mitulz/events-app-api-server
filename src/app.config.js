125ab1d9cb5b27282317f0e5c6ea34961bcf513d
mitulz <mitul16@gmail.com>
2022-04-28 16:46:50 +0000

    Changed name in new-feature-X branch

--- src/app.config.js
+++ src/app.config.js
@@ -2,9 +2,10 @@
  * Place to store data specific to this instance
  * DO NOT store secrets here
  * DO NOT store state here - must be part of a stateless service
+ * Code from the new-feature-X branch
  */
 class AppConfig {
-    team = 'Mitul';
+    team = 'NewCode';
     version = '1.0.0';
 }
 module.exports = AppConfig;
